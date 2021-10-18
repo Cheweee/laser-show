7dd4d1c743ff2437c2bde75d1d0e4ede72887166
Kirill Ivanchenko <K.Ivanchenko@treolan.ru>
2021-10-18 04:57:45 +0300

    #LS4: Рефакторинг секции клиентов

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -10,28 +10,15 @@ import {
     FiberManualRecord
 } from "@mui/icons-material";
 
-import Masonry from '@mui/lab/Masonry';
-import MasonryItem from '@mui/lab/MasonryItem';
 
 import ReactPlayer from "react-player";
 
 import { appStyles } from "../theme";
 import { mergeStyles } from "./utils";
-
-//#region images
-import mvideo from '../assets/clients/mvideo.svg';
-import danone from '../assets/clients/danone.png';
-import porsche from '../assets/clients/porsche.png';
-import renault from '../assets/clients/renault.png';
-import rosbank from '../assets/clients/rosbank.png';
-import s7 from '../assets/clients/s7.png';
-import gedon from '../assets/clients/gedon.jpg';
-import whiskas from '../assets/clients/whiskas.jpg';
 import laserShow3 from '../assets/laser show 3.jpg';
-import clientsBackground from '../assets/laser show 2.jpg';
 import { IntroSection } from "./introSection";
 import { AboutUseSection } from "./aboutUsSection";
-//#endregion
+import { ClientsSection } from "./clientsSection";
 
 const styles = mergeStyles(appStyles);
 
@@ -153,48 +140,9 @@ export const App = withStyles(styles)(function (props: any) {
                     <FiberManualRecord />
                 </IconButton>
             </div>
-            <IntroSection playingVideo={introPlaying} sectionRef={introRef} onScrollDown={scrollToAboutUs} />
+            <IntroSection sectionRef={introRef} playingVideo={introPlaying} onScrollDown={scrollToAboutUs} />
             <AboutUseSection sectionRef={aboutUsRef} />
-            <Grid ref={clientsRef}
-                container
-                alignItems="center"
-                className={`${classes.body} ${classes.intro}`}>
-                <Grid container>
-                    <Grid item className={classes.mr3}>
-                        <Typography variant="h4" component="h4" className={classes.text}>Наши клиенты</Typography>
-                        <div className={`${classes.hl} ${classes.mt3}`} />
-                    </Grid>
-                    <Grid item xs>
-                        <Masonry columns={3} spacing={5}>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={gedon} alt={"GEDON"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${mvideo}?w=162&auto=format`} srcSet={`${mvideo}?w=162&auto=format&dpr=2 2x`} alt={"MVIDEO"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${porsche}?w=162&auto=format`} srcSet={`${porsche}?w=162&auto=format&dpr=2 2x`} alt={"PORSCHE"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${rosbank}?w=162&auto=format`} srcSet={`${rosbank}?w=162&auto=format&dpr=2 2x`} alt={"ROSBANK"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${renault}?w=162&auto=format`} srcSet={`${renault}?w=162&auto=format&dpr=2 2x`} alt={"RENAULT"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${danone}?w=162&auto=format`} srcSet={`${danone}?w=162&auto=format&dpr=2 2x`} alt={"DANONE"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${whiskas}?w=162&auto=format`} srcSet={`${whiskas}?w=162&auto=format&dpr=2 2x`} alt={"WHISKAS"} loading="lazy" />
-                            </MasonryItem>
-                            <MasonryItem>
-                                <img className={classes.clientsItem} src={`${s7}?w=162&auto=format`} srcSet={`${s7}?w=162&auto=format&dpr=2 2x`} alt={"S7"} loading="lazy" />
-                            </MasonryItem>
-                        </Masonry>
-                    </Grid>
-                </Grid>
-                <img className={classes.aboutUsBackground} src={clientsBackground} alt="laser show" />
-            </Grid>
+            <ClientsSection sectionRef={clientsRef}/>
             <Grid container ref={pricesRef} className={classes.h100} direction="row">
                 <Grid xs item
                     container
